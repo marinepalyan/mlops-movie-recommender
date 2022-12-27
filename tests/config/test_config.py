0cca164bca3f6e3eccfe43bb6e1df118eccab474
LianaIs <liana.isayan@mail.ru>
2022-12-27 10:31:27 +0400

    update

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,73 +3,18 @@ from pathlib import Path
 import pytest
 from pydantic import ValidationError
 
-from gb_regressor.config.core import (
+from src.movie_predictor.api.config import (
     create_and_validate_config,
     fetch_config_from_yaml,
 )
 
 TEST_CONFIG_TEXT = """
-package_name: gb_regressor
-training_data_file: houseprice.csv
-test_data_file: test.csv
-drop_features: YrSold
-pipeline_name: gb_regression
-pipeline_save_file: gb_regression_output_v
-target: SalePrice
-variables_to_rename:
-  old: new
-features:
-  - LotArea
-numerical_vars:
-  - LotArea
-categorical_vars:
-  - BsmtQual
-temporal_vars: YearRemodAdd
-numerical_vars_with_na:
-  - LotFrontage
-numerical_na_not_allowed:
-  - LotArea
-test_size: 0.1
-random_state: 0
-n_estimators: 50
-rare_label_tol: 0.01
-rare_label_n_categories: 5
-loss: squared_error
-allowed_loss_functions:
-  - squared_error
-  - absolute_error
-  - huber
+package_name: movie_predictor
+cleaned_data: cleaned_data.csv
+drop_features: ['name', 'year', 'score']
+target: score
+test_size: 0.2
 """
-
-INVALID_TEST_CONFIG_TEXT = """
-package_name: gb_regressor
-training_data_file: houseprice.csv
-test_data_file: test.csv
-drop_features: YrSold
-pipeline_name: gb_regression
-pipeline_save_file: gb_regression_output_v
-target: SalePrice
-features:
-  - LotArea
-numerical_vars:
-  - LotArea
-categorical_vars:
-  - BsmtQual
-temporal_vars: YearRemodAdd
-numerical_vars_with_na:
-  - LotFrontage
-numerical_na_not_allowed:
-  - LotArea
-test_size: 0.1
-random_state: 0
-n_estimators: 50
-rare_label_tol: 0.01
-rare_label_n_categories: 5
-loss: squared_error
-allowed_loss_functions:
-  - huber
-"""
-
 
 def test_fetch_config_structure(tmpdir):  # tmpdir is pytest built-in fixture
     # Given
@@ -107,20 +52,4 @@ def test_config_validation_raises_error_for_invalid_config(tmpdir):
     assert "not in the allowed set" in str(e_info.value)
 
 
-def test_missing_config_field_raises_validation_error(tmpdir):
-    # Given
-    # We make use of the pytest built-in tmpdir fixture
-    configs_dir = Path(tmpdir)
-    sample_config = configs_dir / "sample_config.yml"
-
-    TEST_CONFIG_TEXT = """package_name: gb_regressor"""
-    sample_config.write_text(TEST_CONFIG_TEXT)
-    parsed_config = fetch_config_from_yaml(cfg_path=sample_config)
-
-    # When
-    with pytest.raises(ValidationError) as e_info:
-        create_and_validate_config(parsed_config=parsed_config)
-
-    # Then
-    assert "field required" in str(e_info.value)
-    assert "pipeline_name" in str(e_info.value)
+# removed test_missing_config_field_raises_validation_error(tmpdir) with "field required" and "pipeline_name" 
